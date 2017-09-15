bb9dbc424aa9429590b0ef1045c55515aa9b98f5
EvgeniySher <ecvgeniy.shergunov@gmail.com>
2017-09-15 15:17:58 +0300

    JUNIT

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class CalculateTest {
+	@Test
+	public void when9Multplic4ThanNumber36() {
+		Calculate calc = new Calculate();
+		int expect = 36;
+		int result = calc.cal(9, 4);	
+		assertThat(result, is(expect));		
+}	
+}
