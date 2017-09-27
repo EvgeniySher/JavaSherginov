601d9a915234d666ae4b453c4a9586251595519c
EvgeniySher <ecvgeniy.shergunov@gmail.com>
2017-09-27 11:30:24 +0300

    Работа с точками в пространстве

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -10,13 +10,10 @@ public class Max {
      *
      * @param a = 1
      * @param c = 2
+     * @param b = 3
      * @return = max param
      */
-    public int max(int a, int c) {
-        /**
-         * Я так и не понял за требование к тернарному условию потому сделл тоже самое но через Vf
-         */
-       int maximum = Math.max(a, c);
-            return maximum;
+    public int max(int a, int b, int c) {
+      return Math.max(a, Math.max(b, c));
     }
 }
